7e0b9c33c3e9de00504d0f6e0d6ae68e4671e00d
akshayamdr <akshayamdr@hotmail.com>
2016-08-20 13:22:57 +0545

    updated with mongoDB

--- app.js
+++ app.js
@@ -2,15 +2,38 @@ var express=require('express'),
 	app=express(),
 	server=require('http').createServer(app),
 	io=require('socket.io').listen(server),
-	//nicknames=[];
+	 mongoose=require('mongoose');
 	users={};
 server.listen(3000);
+
+mongoose.connect('mongodb://localhost/chatter', function(err){
+	if(err){
+		console.log(err);
+	} else{
+		console.log('connectedto mongoDB!');
+	}
+});
+
+var chatSchema = mongoose.Schema({
+	nick: String,
+	msg: String,
+	created: {type: Date,default: Date.now}
+});
+
+var Chat = mongoose.model('Message',chatSchema);
 
 app.get('/',function(req,res){
 	res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function(socket){
+	var query = Chat.find({});
+	query.limit(8).exec(function(err, docs){
+		if(err) throw err;
+		console.log('sending old msg!');
+		socket.emit('load old msgs', docs);
+	});
+	
 	socket.on('new user', function(data, callback){
 		if (data in users){
 				callback(false);			
@@ -27,24 +50,30 @@ io.sockets.on('connection', function(socket){
 	}
 	
 	socket.on('send message', function(data, callback){
-		var msg=data.trim();
+		var msg = data.trim(); //trim isnt working
+		console.log('after trimming message is: ' +msg); //working
 		if(msg.substr(0,3)==='/w'){
-			msg=msg.substr(3);
-			var ind=msg.indexOf('  ');
-			if(ind!==-1){
-				var name=msg.substring(0, ind);
-				var msg=msg.substring(ind+1);
-				if(name in users){
+			 msg=msg.substr(3);
+			 var ind=msg.indexOf(' ');
+			 if (ind !== -1){
+				 var name= msg.substring(0, ind);
+				 var msg=msg.substring(ind+1);
+				 if(name in users){
 					users[name].emit('whisper',{msg: msg, nick:socket.nickname});
 					console.log('Whisper!');
-				}else{
-					callbaback('Error! Enter a valid user.');
-				}
-			}else{
-				callback('Error! Please enter a messsage for your whisper.');
-			}
+				 }else{
+					 callback('Error! Enter a valid user.');
+				 }
+			 } else{
+				 callback('Error! Please enter a messsage for your whisper.');
+			 }
+			
 		}else{
-			io.sockets.emit('new message',{msg: msg, nick:socket.nickname});
+			var newMsg = new Chat({msg: msg, nick: socket.nickname});
+			newMsg.save(function(err){
+				if(err) throw err;
+				io.sockets.emit('new message',{msg: msg, nick:socket.nickname});
+				});
 			}
 	});
 	
